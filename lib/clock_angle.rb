ed6a9e24e37aa3345a466f25f39d6cc0f4b1728a
Bang Nguyen and Leah Nelson <leah@>
2016-06-27 15:05:30 -0700

    refactor code

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,13 +1,15 @@
 class Fixnum
   define_method(:clock_angle) do |minute|
     new_min = minute/5.to_f()
-    hour = self
     if new_min == 0
       return 0
-    elsif new_min > hour
-      return ((new_min - hour) * 30).to_i()
     else
-      return ((hour - new_min) * 30).to_i()
+      angle = ((new_min - self) * 30).abs().to_i()
+    end
+    if angle > 180
+      return 360-angle
+    else
+      return angle
     end
   end
 end
